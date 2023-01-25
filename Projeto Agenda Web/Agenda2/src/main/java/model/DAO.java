4a531de76631cbf62b32cd4382841335133604f6
Eric ferreira de souza <eric.vash@hotmail.com>
2023-01-25 16:34:21 -0300

    Ajustes conexao

--- Projeto Agenda Web/Agenda2/src/main/java/model/DAO.java
+++ Projeto Agenda Web/Agenda2/src/main/java/model/DAO.java
@@ -1,5 +1,41 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	/**Módulo de conxão**/
+	// Parâmetros 
+	
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/bdagenda?useTimezone=true&serverTimezone=UTC";
+	private String user ="root";
+	private String password = "Corintios@01";
+	
+	// Método conexão 
+	
+	private Connection conexao() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+			
+		}
+	}
+	
+	// Teste
+	
+	public void testeConexao() {
+		try {
+			Connection con = conexao();
+			System.out.println(con);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+	}
 }
